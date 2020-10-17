eebabb09519617910bab7368309a97e37bf366a6
yn5 <ynsnetmail@gmail.com>
2020-10-17 14:55:45 +0200

    fix(index): type error after updating contentful-ui-extensions-sdk

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,8 @@ import { render } from 'react-dom';
 import {
   AppExtensionSDK,
   SidebarExtensionSDK,
-  BaseExtensionSDK,
   init,
-  locations
+  locations,
 } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss/dist/styles.css';
@@ -15,18 +14,21 @@ import './index.css';
 import Config from './components/ConfigScreen';
 import Sidebar from './components/Sidebar';
 
-init((sdk: BaseExtensionSDK) => {
+init((sdk) => {
   const root = document.getElementById('root');
 
   const ComponentLocationSettings = [
-    { location: locations.LOCATION_APP_CONFIG, component: <Config sdk={sdk as AppExtensionSDK} /> },
+    {
+      location: locations.LOCATION_APP_CONFIG,
+      component: <Config sdk={sdk as AppExtensionSDK} />,
+    },
     {
       location: locations.LOCATION_ENTRY_SIDEBAR,
       component: <Sidebar sdk={sdk as SidebarExtensionSDK} />
     },
   ];
 
-  ComponentLocationSettings.forEach(componentLocationSetting => {
+  ComponentLocationSettings.forEach((componentLocationSetting) => {
     if (sdk.location.is(componentLocationSetting.location)) {
       render(componentLocationSetting.component, root);
     }
